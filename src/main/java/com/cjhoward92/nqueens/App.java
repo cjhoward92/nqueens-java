224e0f48ed92413073550bf6411ca22af7e2072a
Carson Howard <cjhoward92@gmail.com>
2019-09-22 19:42:46 -0700

    Removed log

--- src/main/java/com/cjhoward92/nqueens/App.java
+++ src/main/java/com/cjhoward92/nqueens/App.java
@@ -4,7 +4,6 @@ public class App
 {
     public static void main(String[] args)
     {
-        System.out.println("Setting up the game.");
         Chessboard board = new Chessboard(8);
         Solutions solutions = board.solve();
         solutions.print();
